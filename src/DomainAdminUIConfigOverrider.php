a4b7c6369ca7bfb2af6c4373619f2d5221b9db32
Herjan van Eijk <herjan.van.eijk@ndcmediagroep.nl>
2018-06-15 12:49:32 +0200

    Overwrite the loadOverrides() function to make UI selection leading

--- src/DomainAdminUIConfigOverrider.php
+++ src/DomainAdminUIConfigOverrider.php
@@ -2,6 +2,8 @@
 
 namespace Drupal\domain_admin_ui;
 
+use Drupal\Core\Config\StorageInterface;
+use Drupal\domain\DomainNegotiatorInterface;
 use Drupal\domain_config\DomainConfigOverrider;
 use Drupal\domain\DomainInterface;
 use Drupal\Core\Language\LanguageInterface;
@@ -10,6 +12,16 @@ use Drupal\Core\Language\LanguageInterface;
  * Extend DomainConfigOverrider to allow domain to be set.
  */
 class DomainAdminUIConfigOverrider extends DomainConfigOverrider {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function __construct(StorageInterface $storage, DomainNegotiatorInterface $domainNegotiator) {
+    parent::__construct($storage);
+
+    $this->domainNegotiator = $domainNegotiator;
+  }
+
   /**
    * {@inheritDoc}
    * @see \Drupal\domain_config\DomainConfigOverrider::getDomainConfigName()
@@ -35,4 +47,23 @@ class DomainAdminUIConfigOverrider extends DomainConfigOverrider {
   public function setLanguage(LanguageInterface $language) {
     $this->language = $language;
   }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function loadOverrides($names) {
+    // This function is overridden because the 'parent::loadOverrides' cannot be
+    // manipulated any other way. It's uses the domainNegotiator with a hard
+    // refresh (see DomainConfigOverride::initiateContext) which results in the
+    // current domain being loaded. When the current domain cannot be found it
+    // will fallback to the default domain. Either way, the config will be
+    // overwritten by a domain. To prevent this, just return an empty array.
+    // This way the selected domain will be leading for the loaded configuration.
+    if (empty($this->domainNegotiator) || !$this->domainNegotiator->getSelectedDomainId()) {
+      return [];
+    }
+
+    return parent::loadOverrides($names);
+  }
+
 }
